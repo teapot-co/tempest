79bf8c60326279941390dcd4176bea51f14e3dd6
Peter Lofgren <peterl@stripe.com>
2016-12-15 11:54:54 -0800

    Add support for specifying source and target node type in PPR call

--- src/main/scala/co/teapot/tempest/algorithm/MonteCarloPPR.scala
+++ src/main/scala/co/teapot/tempest/algorithm/MonteCarloPPR.scala
@@ -14,7 +14,7 @@
 
 package co.teapot.tempest.algorithm
 
-import co.teapot.tempest.graph.DirectedGraph
+import co.teapot.tempest.graph.{DirectedGraph, EdgeDir}
 import co.teapot.tempest.MonteCarloPageRankParams
 import co.teapot.tempest.util.CollectionUtil
 
@@ -23,26 +23,41 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Random
 
+
 /**
   * Performing Monte Carlo walks from a list of seed nodes (specified by their ids)
   */
 object MonteCarloPPR {
-  def estimatePPR(graph: DirectedGraph, startIds: IndexedSeq[Int], params: MonteCarloPageRankParams,
+  def estimatePPR(graph: DirectedGraph,
+                  startIds: IndexedSeq[Int],
+                  firstStepDirection: EdgeDir,
+                  lengthConstraint: LengthConstraint,
+                  params: MonteCarloPageRankParams,
                   random: Random = new Random()):
   collection.Map[Int, Double] = {
     val pprs = CollectionUtil.efficientIntDoubleMapWithDefault0()
     def randomStart(): Int = startIds(random.nextInt(startIds.size))
+
+    var currentDirection = firstStepDirection
+    var currentLength = 0
     var v = randomStart()
+
     for (stepIndex <- 0 until params.getNumSteps) {
-      pprs(v) += 1.0 / params.getNumSteps
-      if (random.nextDouble() < params.getResetProbability) {
+      if (lengthConstraint.isAllowed(currentLength)) {
+        pprs(v) += 1.0 / params.getNumSteps
+      }
+
+      val vDegree = graph.degree(v, currentDirection)
+      if (random.nextDouble() < params.getResetProbability || vDegree == 0) {
         v = randomStart()
+        currentDirection = firstStepDirection
+        currentLength = 0
       } else {
-        val vDegree = graph.outDegree(v)
-        if (vDegree > 0)
-          v = graph.outNeighbor(v, random.nextInt(vDegree))
-        else
-          v = randomStart()
+        v = graph.neighbor(v, random.nextInt(vDegree), currentDirection)
+        if (params.alternatingWalk) {
+          currentDirection = currentDirection.flip
+        }
+        currentLength += 1
       }
     }
     if (params.isSetMinReportedVisits) {
@@ -58,17 +73,36 @@ object MonteCarloPPR {
     }
   }
 
-  def estimatePPRParallel(graph: DirectedGraph, startIds: IndexedSeq[Int], params:
-  MonteCarloPageRankParams, threadCount: Int):
+  def estimatePPRParallel(graph: DirectedGraph,
+                          startIds: IndexedSeq[Int],
+                          firstStepDirection: EdgeDir,
+                          lengthConstraint: LengthConstraint,
+                          params: MonteCarloPageRankParams,
+                          threadCount: Int):
   collection.Map[Int, Double] = {
     params.setNumSteps(params.getNumSteps / threadCount)
+    params.setMinReportedVisits(params.getMinReportedVisits / threadCount)
     val pprMapFutures = (0 until threadCount) map { i =>
       Future[collection.Map[Int, Double]] {
-        estimatePPR(graph, startIds, params)
+        estimatePPR(graph, startIds, firstStepDirection, lengthConstraint, params)
       }
     }
 
     val pprMaps = Await.result(Future.sequence(pprMapFutures), Duration.Inf)
     CollectionUtil.mean(pprMaps)
+  }
+
+
+  sealed abstract class LengthConstraint {
+    def isAllowed(length: Int): Boolean
+  }
+  case object EvenLength extends LengthConstraint {
+    def isAllowed(length: Int): Boolean = length % 2 == 0
+  }
+  case object OddLength extends LengthConstraint {
+    def isAllowed(length: Int): Boolean = length % 2 == 1
+  }
+  case object AnyLength extends LengthConstraint {
+    def isAllowed(length: Int): Boolean = true
   }
 }
