30a1cc6bb2bb4d018a8f98ca620ce2e04433dbae
Peter Lofgren <peterl@stripe.com>
2017-01-27 12:47:30 -0800

    Suppress debug log messages (produced by postgres client library

--- src/main/scala/co/teapot/tempest/util/LogUtil.scala
+++ src/main/scala/co/teapot/tempest/util/LogUtil.scala
@@ -1,14 +1,16 @@
 package co.teapot.tempest.util
 
 import com.twitter.logging.{ConsoleHandler, Logger, LoggerFactory}
-import org.apache.log4j.{DailyRollingFileAppender, PatternLayout, BasicConfigurator}
+import org.apache.log4j.{BasicConfigurator, DailyRollingFileAppender, PatternLayout, Level, Logger => Logger4j}
 
 object LogUtil {
   def configureLog4j(): Unit = {
     // Thrift uses log4j, which complains if logging is not configured
     BasicConfigurator.configure() // write logs to stdout as well
+    Logger4j.getRootLogger.setLevel(Level.INFO)
     val layout = new PatternLayout("%-5p %d [%t]: %m%n")
     val appender = new DailyRollingFileAppender(layout, "main_log4j.log", "'.'yyyy-MM-dd")
+    appender.setThreshold(Level.INFO)
     BasicConfigurator.configure(appender)
   }
 
